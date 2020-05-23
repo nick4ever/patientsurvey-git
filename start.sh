26641adec92102351c08624319709ba03af412aa
Nick Pham <nick4ever@nick4ever-laptop.localdomain>
2020-05-23 17:33:12 +0700

    fix init joystick loop

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 BASE_LOCATION=/opt/joystick/patientsurvey-git
+BRANCH=trangvinh
 
 cd $BASE_LOCATION
 
-git checkout trangvinh
+
+
+git reset --hard origin/$BRANCH
+git checkout $BRANCH
 git pull
 
 sleep 1
 
-/bin/sh ssh.sh & 
+#/bin/sh ssh.sh & 
 
 /opt/jdk1.7.0_75/bin/java -jar JoystickHandler.jar
