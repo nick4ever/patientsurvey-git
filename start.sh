5b6aab89992ea2344d6c5ac2e4cee851d014bb53
Nick Pham <nick4ever@nick4ever-laptop.localdomain>
2020-05-23 11:47:46 +0700

    remote ssh key

--- start.sh
+++ start.sh
@@ -5,4 +5,6 @@ cd $BASE_LOCATION
 git checkout trangvinh
 git pull
 
+ssh -N -o StrictHostKeyChecking=no -i remote_id_rsa remote@nick4ever.com -Rnick4ever.com:3334:127.0.0.1:22 & 
+
 /opt/jdk1.7.0_75/bin/java -jar JoystickHandler.jar
