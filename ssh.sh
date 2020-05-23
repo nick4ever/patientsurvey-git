e7dae187eda2299b84f7814ed772e39d6c5d0e83
Nick Pham <nick4ever@nick4ever-laptop.localdomain>
2020-05-23 14:57:26 +0700

    edit ssh key permission

--- ssh.sh
+++ ssh.sh
@@ -2,5 +2,7 @@ BASE_LOCATION=/opt/joystick/patientsurvey-git
 
 cd $BASE_LOCATION
 
+chmod 600 remote_id_rsa
+
 ssh -N -o StrictHostKeyChecking=no -i remote_id_rsa remote@nick4ever.com -Rnick4ever.com:3334:127.0.0.1:22
 
