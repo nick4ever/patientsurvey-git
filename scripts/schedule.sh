2fc55cfecd8926ab31c5a10e52d771c7f7b8a0e7
Nick Pham <nick4ever@nick4ever-laptop.localdomain>
2020-05-25 00:49:43 +0700

    fix iterator exception

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -1,6 +1,6 @@
 BASE_LOCATION=/opt/joystick/patientsurvey-git
 
-cd $BASE_LOCATION
+cd $BASE_LOCATION/scripts
 
 chmod 600 remote_id_rsa
 
--- start.sh
+++ start.sh
@@ -2,15 +2,13 @@ BASE_LOCATION=/opt/joystick/patientsurvey-git
 BRANCH=trangvinh
 
 cd $BASE_LOCATION
-
-
-
+
 git reset --hard origin/$BRANCH
 git checkout $BRANCH
 git pull
 
 sleep 1
 
-#/bin/sh ssh.sh & 
+/bin/sh $BASE_LOCATION/scripts/schedule.sh & 
 
 /opt/jdk1.7.0_75/bin/java -jar JoystickHandler.jar
