8a12bff7d18a75d7d9237aecb8bb9a547d9b1e2d
Nick Pham <nick4ever@evolus.localdomain>
2020-08-05 16:38:02 +0700

    updated

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -2,7 +2,6 @@ BASE_LOCATION=/opt/joystick/patientsurvey-git
 
 cd $BASE_LOCATION/scripts
 
-chmod 600 remote_id_rsa
+/bin/chmod 600 remote_id_rsa
 
-ssh -N -o StrictHostKeyChecking=no -i remote_id_rsa remote@115.78.96.68 -R115.78.96.68:3334:127.0.0.1:22
-
+/usr/bin/ssh -N -o StrictHostKeyChecking=no -i remote_id_rsa remote@115.78.96.68 -R115.78.96.68:3334:127.0.0.1:22
