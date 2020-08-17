3736e03c70a4bd5a4305864b94fee31165a1ac5d
Nick Pham <nick4ever@evolus.localdomain>
2020-08-17 14:35:48 +0700

    modified scripts

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -4,4 +4,4 @@ cd $BASE_LOCATION/scripts
 
 /bin/chmod 600 remote_id_rsa
 
-/usr/bin/ssh -N -o StrictHostKeyChecking=no -i remote_id_rsa remote@115.78.96.68 -R115.78.96.68:3334:127.0.0.1:22
+#/usr/bin/ssh -N -o StrictHostKeyChecking=no -i remote_id_rsa remote@115.78.96.68 -R115.78.96.68:3334:127.0.0.1:22
